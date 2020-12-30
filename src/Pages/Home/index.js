ce3eb481a0ae2416b2b6df6e5b2887c11d964491
sangya2001 <sangyasherpa2058@gmail.com>
2020-12-31 01:07:33 +0545

    Added top sales and made responsive

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,21 +1,45 @@
 import React, { Suspense } from "react";
 import "./style.css";
 
+// constants demo
+import { topSales } from "../../Utils/ProductDetails";
+
 // components
 const Navbar = React.lazy(() => import("../../Components/Navbar"));
 const Landing = React.lazy(() => import("../../Components/Home/Landing"));
+const ProductCard = React.lazy(() =>
+  import("../../Components/Home/ProductCard")
+);
 
 const index = () => {
   return (
     <div className="home">
+      {/* 1. Navbar */}
       <Suspense fallback={<div>Nav Loading...</div>}>
         <Navbar />
       </Suspense>
 
       <div className="scrollableHome">
+        {/* 2. Landing */}
         <Suspense fallback={<div>This is Landing...</div>}>
           <Landing />
         </Suspense>
+
+        {/* 3. Top Sales */}
+        <h2>Top Sales</h2>
+        <div className="topSales">
+          {topSales.map((item, index) => (
+            <ProductCard key={index} productInfo={item} />
+          ))}
+        </div>
+
+        {/* 4. Recent Items */}
+        <h2>Recent Items</h2>
+        <div className="recentItems">
+          {topSales.map((item, index) => (
+            <ProductCard key={index} productInfo={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/Utils/ProductDetails.js
@@ -0,0 +1,44 @@
+export const topSales = [
+    {
+        id: 1,
+        productName: "Ficus Bonsai",
+        productDescription: "Ficus Bonsai is one of the luxuriest, best for gift and best collection plant to have!",
+        productImage: "https://i2.wp.com/paudheseyaari.com/wp-content/uploads/2019/06/DSC_0564.jpg",
+        productPrice: 2.15
+    },
+    {
+        id: 2,
+        productName: "Hoya Heart",
+        productDescription: "Hoya Heart is also a succulent species which is chosen best as gift plant.",
+        productImage: "https://cdn.shopify.com/s/files/1/0108/5782/products/hoyaheart-michlers_2048x.jpg",
+        productPrice: 1.56
+    },
+    {
+        id: 3,
+        productName: "Chain of Hearts",
+        productDescription: "Chain of Hearts is one of the easiest plant grow which needs lesser maintainance.",
+        productImage: "https://door2doorplants.co/wp-content/uploads/2020/03/IMG_0647-1-scaled.jpg",
+        productPrice: 2.15
+    },
+    {
+        id: 4,
+        productName: "Chain of Hearts",
+        productDescription: "Chain of Hearts is one of the easiest plant grow which needs lesser maintainance.",
+        productImage: "https://door2doorplants.co/wp-content/uploads/2020/03/IMG_0647-1-scaled.jpg",
+        productPrice: 2.15
+    },
+    {
+        id: 5,
+        productName: "Chain of Hearts",
+        productDescription: "Chain of Hearts is one of the easiest plant grow which needs lesser maintainance.",
+        productImage: "https://door2doorplants.co/wp-content/uploads/2020/03/IMG_0647-1-scaled.jpg",
+        productPrice: 2.15
+    },
+    {
+        id: 6,
+        productName: "Chain of Hearts",
+        productDescription: "Chain of Hearts is one of the easiest plant grow which needs lesser maintainance.",
+        productImage: "https://door2doorplants.co/wp-content/uploads/2020/03/IMG_0647-1-scaled.jpg",
+        productPrice: 2.15
+    },
+]
